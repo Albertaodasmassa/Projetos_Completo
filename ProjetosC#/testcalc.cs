b11a78275c74d4f6b109e6da2ebe6bb576625c2f
Sid <albertaodasmassa@gmail.com>
2024-12-22 13:27:45 -0300

    Update testcalc.cs

--- ProjetosC#/testcalc.cs
+++ ProjetosC#/testcalc.cs
@@ -13,11 +13,14 @@ class Program
     Console.WriteLine("Selecione uma opcao 1 ou 2");
     int opcao = Convert.ToInt32(Console.ReadLine());
     switch(opcao){
-      case 1:
+     case 1:
       Console.Write("Digite a taxa de Juros do titulo em %:");
       decimal taxaC = Convert.ToDecimal(Console.ReadLine())/100;
-      resultado = principal * (decimal)Math.Pow((double)(1 + taxaC), tempo);
-      Console.WriteLine($"O valor acumulado em {tempo} anos com juros compostos: {resultado:C}");
+      Console.Write("Digite a taxa do IPCA atual:");
+      decimal taxaipca = Convert.ToDecimal(Console.ReadLine())/100;
+      decimal txa = taxaC + taxaipca;
+      resultado = principal * (decimal)Math.Pow((double)(1 + txa), tempo);
+      Console.WriteLine($"O valor acumulado em {tempo} anos, com juros do ativo sera R$: {resultado:F2}");
       break;
       default:
       Console.WriteLine("Opção inválida.");
