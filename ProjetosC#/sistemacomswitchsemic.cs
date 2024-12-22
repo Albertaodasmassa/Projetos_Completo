3028ee7c35fb51b1fb050216f91dd7baaf4c569e
Sid <albertaodasmassa@gmail.com>
2024-12-22 10:15:20 -0300

    Update sistemacomswitchsemic.cs

--- ProjetosC#/sistemacomswitchsemic.cs
+++ ProjetosC#/sistemacomswitchsemic.cs
@@ -15,12 +15,32 @@ namespace Teste
         }
         static void dimi()
         {
+            Console.WriteLine("Por favor Informe um numero");
+            int v3 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Por favor Informe um numero");
+            int v4 = Convert.ToInt16(Console.ReadLine());
+            int res;
+            res=v3-v4;
+            Console.WriteLine("O resultado é {0}", res);
+        }
+        static void mult()
+        {
             Console.WriteLine("Por favor Informe o primeiro Numero");
             int v3 = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Por favor Informe o primeiro Numero");
             int v4 = Convert.ToInt16(Console.ReadLine());
             int res;
-            res=v3-v4;
+            res=v3*v4;
+            Console.WriteLine("O resultado é {0}", res);
+        }
+        static void div()
+        {
+            Console.WriteLine("Por favor Informe o primeiro Numero");
+            int v3 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Por favor Informe o primeiro Numero");
+            int v4 = Convert.ToInt16(Console.ReadLine());
+            int res;
+            res=v3/v4;
             Console.WriteLine("O resultado é {0}", res);
         }
         static void Main()
@@ -28,6 +48,7 @@ namespace Teste
             int menu;
             Console.Write("Digite uma opção de 1 a 3: ");
             menu=Convert.ToInt16(Console.ReadLine());
+            while(menu!=4){
             switch(menu)
             {
             case 1:
@@ -36,10 +57,17 @@ namespace Teste
             case 2:
                 dimi(); //chama a função similar a JS
                 break;
+            case 3:
+                mult(); //chama a função similar a JS
+                break; 
+            case 4:
+                div(); //chama a função similar a JS
+                break; 
             default:
                 Console.WriteLine("Opção inválida.");
                 break;
             }
         }
     }
+}
 }
